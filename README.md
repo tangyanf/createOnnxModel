bd1af6180864ac25e7850baad972820a22766936
tangyanfei <tangyanfei@sensetime.com>
2021-04-08 20:18:33 +0800

    update code, add try

--- README.md
+++ README.md
@@ -1,2 +1,73 @@
 # createOnnxModel
 a tool to convert mmlab model to onnx
+
+## requirement
+* python >= 3.5
+* pytorch == 1.6
+  ```shell
+  pip install torch==1.6.0 torchvision==0.7.0
+  ```
+* onnxruntime==1.5.1
+  ```shell
+  pip install onnxruntime==1.5.1
+  ```
+* onnx
+  ```shell
+  pip install onnx
+  ```
+* onnx-simplifier
+  ```shell
+  pip install onnx-simplifier
+  ```
+* mmcv-full
+
+    to install mmcv-full with custom ops, we need to download onnxruntime library, And add it into environment
+    1. download onnxruntime-1.5.1
+    ```shell
+    wget https://github.com/microsoft/onnxruntime/releases/download/v1.5.1/onnxruntime-linux-x64-1.5.1.tgz
+    
+    tar -zxvf onnxruntime-linux-x64-1.5.1.tgz
+    ```
+    2. add environment variable
+    ```shell
+    cd onnxruntime-linux-x64-1.5.1
+    export ONNXRUNTIME_DIR=$(pwd)
+    export LD_LIBRARY_PATH=$ONNXRUNTIME_DIR/lib:$LD_LIBRARY_PATH
+    ```
+    3. build on linux
+    ```shell
+    git clone https://github.com.cnpmjs.org/open-mmlab/mmcv.git
+    cd mmcv
+    MMCV_WITH_OPS=1 MMCV_WITH_ORT=1 pip install -e .
+    ```
+* mmdetection
+  ```shell
+  git clone https://github.com.cnpmjs.org/open-mmlab/mmdetection.git
+  cd mmdetection
+  python setup.py install
+  ```
+
+* mmclassification
+  ```shell
+  git clone https://github.com.cnpmjs.org/open-mmlab/mmclassification.git
+  cd mmclassification
+  python setup.py install
+  ```
+* mmediting
+  ```shell
+  git clone https://github.com.cnpmjs.org/open-mmlab/mmediting.git
+  cd mmediting
+  python setup.py install
+  ```
+* mmsegmentation
+  ```shell
+  git clone https://github.com.cnpmjs.org/open-mmlab/mmsegmentation.git
+  cd mmsegmentation
+  python setup.py install
+  ```
+
+
+## usage
+```shell
+python createOnnx.py --
+```
--- createOnnx.py
+++ createOnnx.py
@@ -6,13 +6,11 @@ from edit2onnx import convertEdit2Onnx
 from seg2onnx import convertSeg2Onnx
 def parse_args():
     parser = argparse.ArgumentParser(description='convert mmlab model to ONNX')
-    parser.add_argument('--detection', action='store_true', help='convert detection model')
-    parser.add_argument('--classification', action='store_true', help='convert classification model')
-    parser.add_argument('--editing', action='store_true', help='convert editing model')
-    parser.add_argument('--segmentation', action='store_true', help='convert segmentation model')
+    parser.add_argument('--class-name', type=str, help='which model class you want to convert, such as: detection/classification/editing/segmentation')
     parser.add_argument('--config', help='config file path')
     parser.add_argument('--checkpoint', help='checkpoint file path')
     parser.add_argument('--dynamic-shape', action='store_true', help='whether to export onnx with dynamic shape')
+    parser.add_argument('--onnx-name', type=str, help='onnx model file name')
     parser.add_argument('--simplify', action='store_true', help='whether to simplify onnx model')
     args = parser.parse_args()
     return args
@@ -25,29 +23,65 @@ if __name__ == '__main__':
     std = [58.395, 57.12, 57.375]
     merged_img = os.path.join(os.path.dirname(__file__), './data/merged/GT05.jpg')
     trimap_img = os.path.join(os.path.dirname(__file__), './data/merged/GT05.jpg')
-    if args.detection:
-        normalize_cfg = {'mean': mean, 'std': std}
-        convertDetection2Onnx(args.config,
-                              args.checkpoint,
-                              input_img,
-                              input_shape,
-                              opset_version=11,
-                              output_file='tmp.onnx',
-                              verify=False,
-                              normalize_cfg=normalize_cfg,
-                              dynamic_export=args.dynamic_shape,
-                              do_simplify=args.simplify)
-    if args.classification:
-        convertCls2Onnx(args.config,
-                        args.checkpoint,
-                        input_shape)
-    if args.editing:
-        convertEdit2Onnx(args.config,
-                         args.checkpoint,
-                         merged_img,
-                         trimap_img)
+    try:
+        if args.class_name is None:
+            raise RuntimeError()
+    except RuntimeError:
+        print('please use --class-name to chose which model class to convert, such as: --class-name mmdetection, --class-name mmclassification, --class-name mmediting, --class-name mmsegmentation')
+        exit(0)
+    try:
+        if args.config is None:
+            raise RuntimeError()
+    except RuntimeError:
+        print('please use --config xxx.pth to chose config file')
+        exit(0)
+    try:
+        if args.checkpoint is None:
+            raise RuntimeError()
+    except RuntimeError:
+        print('please use --checkpoint xxx.checkpoint to chose checkpoint file')
+        exit(0)
 
-    if args.segmentation:
-        convertSeg2Onnx(args.config,
-                        args.checkpoint,
-                        input_shape)
+    out_file = args.onnx_name
+    if out_file is None:
+        out_file = 'tmp.onnx'
+        print('onnx model name is default: tmp.onnx')
+
+    try:
+        if args.class_name == 'detection':
+            normalize_cfg = {'mean': mean, 'std': std}
+            convertDetection2Onnx(args.config,
+                                  args.checkpoint,
+                                  input_img,
+                                  input_shape,
+                                  opset_version=11,
+                                  output_file=out_file,
+                                  verify=False,
+                                  normalize_cfg=normalize_cfg,
+                                  dynamic_export=args.dynamic_shape,
+                                  do_simplify=args.simplify)
+        elif args.class_name == 'classification':
+            convertCls2Onnx(args.config,
+                            args.checkpoint,
+                            input_shape,
+                            output_file=out_file)
+        elif args.class_name == 'editing':
+            convertEdit2Onnx(args.config,
+                             args.checkpoint,
+                             merged_img,
+                             trimap_img,
+                             output_file=out_file)
+
+        elif args.class_name == 'segmentation':
+            convertSeg2Onnx(args.config,
+                            args.checkpoint,
+                            input_shape,
+                            output_file=out_file)
+        else:
+            raise RuntimeError()
+    except RuntimeError:
+        print('class name {0} is not support, please use: {1}, {2}, {3}, {4}'.format(args.class_name,
+                                                                                     'detection',
+                                                                                     'classification',
+                                                                                     'editing',
+                                                                                     'segmentation'))
