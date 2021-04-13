270580fac4b746258c7beab0573d600cb95ba5e6
tangyanfei <tangyanfei@sensetime.com>
2021-04-13 12:27:09 +0800

    update setup.py

--- setup.py
+++ setup.py
@@ -3,16 +3,24 @@ from setuptools import setup, find_packages
 setup(
     name='createOnnx',
     version='0.0.1',
-    install_requires = ['onnx',
-                        'onnx-simplifier',
-                        'torch==1.6.0',
-                        'torchvision==0.7.0',
-                        'mmcls',
-                        'mmdet',
-                        'mmedit',
-                        'mmsegmentation'],
+    packages=find_packages(),
+    package_data={
+        'createOnnx': ['data/*.jpg',
+                       'data/gt/*.png',
+                       'data/lq/*.png',
+                       'data/merged/*.jpg',
+                       'data/trimap/*.png']
+    },
+    install_requires=['onnx',
+                      'onnx-simplifier',
+                      'torch==1.6.0',
+                      'torchvision==0.7.0',
+                      'mmcls',
+                      'mmdet',
+                      'mmedit',
+                      'mmsegmentation'],
 
     entry_points={
-        'console_scripts':['createOnnx=createOnnx.createOnnx:main',]
+        'console_scripts': ['createOnnx=createOnnx.createOnnx:main']
     }
 )
